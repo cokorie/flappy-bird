9d54b3e6a884df98a16ef7efd24a9ab8aba3c3e0
cokorie <chijioke.okorie@gmail.com>
2025-12-15 17:54:24 -0600

    move board params into load function

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ let board;
 let board_w = 400;
 let board_h = 600;
 
-board = document.getElementById("board");
-board.width = board_w;
-board.height = board_h;
+window.onload = function() {
+    board = document.getElementById("board");
+    board.width = board_w;
+    board.height = board_h;
+}
