8103a2fc2599390805478fc1b89242b91748ad70
cokorie <chijioke.okorie@gmail.com>
2025-12-17 12:49:39 -0600

    add bg drawing to update

--- index.js
+++ index.js
@@ -42,11 +42,13 @@ window.onload = function() {
 
 function update() {
     requestAnimationFrame(update);
+
+    // for drawing background
+    context.drawImage(bg_img, bg_x_pos, 0, 400, 600);
+    context.drawImage(bg_img, bg_x_pos + 399, 0, 400, 600);
+
 }
 
-// for drawing background
-context.drawImage(bg_img, bg_x_pos, 0, 400, 600);
-context.drawImage(bg_img, bg_x_pos + 399, 0, 400, 600);
 
 // for drawing player
 context.drawImage(player_img, player.x, player.y, player.width, player.height);
