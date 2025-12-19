00fdd935b1cafad49f8e136147308870f1c4db1f
cokorie <chijioke.okorie@gmail.com>
2025-12-19 07:33:50 -0600

    add pipe_down img call

--- index.js
+++ index.js
@@ -67,7 +67,7 @@ window.onload = function() {
         context.drawImage(pipe_up_img, 200, 200, 79, 300);
     }
 
-    
+    pipe_down_img = new Image();
 
     requestAnimationFrame(update);
     document.addEventListener("keydown", jump);
