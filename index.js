cfb07c8d39a7357a288d3ca801556424d0b7ea6d
cokorie <chijioke.okorie@gmail.com>
2025-12-22 06:44:57 -0600

    add checkCollision note

--- index.js
+++ index.js
@@ -204,6 +204,7 @@ function getRandonInt() {
     return Math.floor(Math.random() * (max - min)) + min;
 }
 
+// for checking player collision with pipes, floor or sky
 function checkCollision(x1, y1, w1, h1, x2, y2, w2, h2) {
     return x1 < x2+w2 && x2 < x1+w1 && y1 < y2+h2 && y2 < y1+h1;
 }
